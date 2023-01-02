b7e15f65bf43687c3a240d8a433be50b17047670
Ansh Bhalala <self@anshb.dev>
2023-01-01 19:13:48 -0700

    Under construction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Hey this is Ansh!</h1>
+<p>This site is under construction rn 👷</p>
