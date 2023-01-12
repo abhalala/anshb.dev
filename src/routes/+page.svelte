c2e7d86901006230254bad0dda773f696ada85d8
Ansh Bhalala <abhalala@asu.edu>
2023-01-12 19:27:14 +0000

    Some minor updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <h1>Hey this is Ansh!</h1>
 <p>This site is under construction rn 👷</p>
 
+<p>I am planning to complete this before end of February</p>
+
 <a href="https://github.com/abhalala/anshb.dev">Here's an link to the github repo</a>
